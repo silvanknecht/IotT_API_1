0f726512768b0978ed53ceb1f2cf5d7be3374be6
enjoymrban <knecht.silvan@gmail.com>
2019-04-04 16:33:10 +0200

    added relais control

--- routes/relais.js
+++ routes/relais.js
@@ -3,26 +3,26 @@ const router = express.Router();
 
 
 // import controllers
-const LedController = require("../controllers/LedController");
+const RelaisController = require("../controllers/RelaiController");
 
 router.get("/", (req, res, next) => {
-  res.send("all states of the leds");
+  res.send("all states of the relais");
 });
 
 router.post("/on/:id", (req, res, next) => {
   let id = req.params.id;
-  if ((id >= 1) & (id < 8)) {
-    LedController.turnOn(id);
-    res.send("Led with Id: " + id + "turned on");
+  if ((id >= 1) & (id < 2)) {
+    RelaisController.turnOn(id);
+    res.send("Relais with Id: " + id + "turned on");
   } else {
     res.status(400).send("Invalid Id");
   }
 });
 router.post("/off/:id", (req, res, next) => {
   let id = req.params.id;
-  if ((id >= 1) & (id < 8)) {
-    LedController.turnOff(id);
-    res.send("Led with Id: " + id + "turned off");
+  if ((id >= 0) & (id < 2)) {
+    RelaisController.turnOff(id);
+    res.send("Relais with Id: " + id + "turned off");
   } else {
     res.status(400).send("Invalid Id");
   }
