9465cc837d16ae9c024cf18cb881ba084daa7382
enjoymrban <knecht.silvan@gmail.com>
2019-04-04 16:12:51 +0200

    changed imports

--- controllers/LedController.js
+++ controllers/LedController.js
@@ -1,10 +1,11 @@
-
+const pfio = require("piface");
+pfio.init();
 
 function turnOn(id) {
     pfio.digital_write(id, 1);
     console.log("turn on: " + pfio.digital_read(id));
   }
-  
+
   function turnOff(id) {
     pfio.digital_write(id, 0);
     console.log("turn off: " + pfio.digital_read(id));
--- routes/leds.js
+++ routes/leds.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const pfio = require("piface");
-pfio.init();
+
 
 // import controllers
 const LedController = require("../controllers/LedController");
