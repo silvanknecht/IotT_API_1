15c9488ee1f1289e5176add7254e8150e487f141
enjoymrban <knecht.silvan@gmail.com>
2019-04-04 16:08:51 +0200

    controller initiated

--- controllers/LedController.js
+++ controllers/LedController.js
@@ -1,3 +1,16 @@
 
-// module.exports.turnOn = turnOn;
-// module.exports.turnOff = turnOff;
+
+function turnOn(id) {
+    pfio.digital_write(id, 1);
+    console.log("turn on: " + pfio.digital_read(id));
+  }
+  
+  function turnOff(id) {
+    pfio.digital_write(id, 0);
+    console.log("turn off: " + pfio.digital_read(id));
+  }
+
+
+
+ module.exports.turnOn = turnOn;
+ module.exports.turnOff = turnOff;
--- routes/leds.js
+++ routes/leds.js
@@ -1,5 +1,4 @@
 const express = require("express");
-//const router = require('express-promise-router');
 const router = express.Router();
 const pfio = require("piface");
 pfio.init();
@@ -7,38 +6,27 @@ pfio.init();
 // import controllers
 const LedController = require("../controllers/LedController");
 
-router.get("/leds", (req, res, next) => {
+router.get("/", (req, res, next) => {
   res.send("all states of the leds");
 });
 
-router.post("/leds/on/:id", (req, res, next) => {
+router.post("/on/:id", (req, res, next) => {
   let id = req.params.id;
   if ((id >= 0) & (id < 8)) {
-    turnOn(id);
+    LedController.turnOn(id);
     res.send("Led with Id: " + id + "turned on");
   } else {
     res.status(400).send("Invalid Id");
   }
 });
-router.post("/leds/off/:id", (req, res, next) => {
+router.post("/off/:id", (req, res, next) => {
   let id = req.params.id;
   if ((id >= 0) & (id < 8)) {
-    turnOff(id);
+    LedController.turnOff(id);
     res.send("Led with Id: " + id + "turned off");
   } else {
     res.status(400).send("Invalid Id");
   }
 });
-
-
-function turnOn(id) {
-  pfio.digital_write(id, 1);
-  console.log("turn on: " + pfio.digital_read(id));
-}
-
-function turnOff(id) {
-  pfio.digital_write(id, 0);
-  console.log("turn off: " + pfio.digital_read(id));
-}
 
 module.exports = router;
